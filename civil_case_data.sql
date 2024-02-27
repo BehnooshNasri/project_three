f2aed0a1ab80c4f3503b2a51badd093c2217c9e3
BehnooshNasri <behnoosh.nasri@gmail.com>
2024-02-26 21:55:50 -0500

    closing the loop - first attempt

--- civil_case_data.sql
+++ civil_case_data.sql
@@ -1,10 +1,14 @@
-﻿DROP TABLE IF EXISTS civil_case_data;
+DROP TABLE IF EXISTS civil_case_data;
 
 CREATE TABLE "civil_case_data" (
+    "ID" int   NOT NULL,
     "Year_Period" varchar(100)   NOT NULL,
     "Jurisdiction" varchar(100)   NOT NULL,
     "Case_Type" varchar(100),
-    "Case_Count" int   
+    "Case_Count" int,
+    CONSTRAINT "pk_civil_case_data" PRIMARY KEY (
+        "ID"
+     )
 );
 
 SELECT * FROM civil_case_data;
