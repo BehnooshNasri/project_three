1970a10e6b28598968910bf41cf7f4de29a1a39c
BehnooshNasri <behnoosh.nasri@gmail.com>
2024-02-28 20:59:53 -0500

    updates to html and js

---
+++ static/js/plots.js
@@ -0,0 +1,24 @@
+url = '/api/v1.0/civil_court_data'
+
+d3.json(url).then(generateFullChart)
+
+function generateFullChart(response) {
+
+   
+
+}
+
+d3.json(url).then(generateCaseTypeChart)
+
+function generateCaseTypeChart(response) {
+
+   
+}
+
+
+d3.json(url).then(generateJurisdictionChart)
+
+function generateJurisdictionChart(response) {
+
+   
+}
