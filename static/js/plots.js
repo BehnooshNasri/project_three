b46a0d97857b1d734e23edec8b9687ce2f7436cd
BehnooshNasri <behnoosh.nasri@gmail.com>
2024-02-28 21:50:56 -0500

    update to the js

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,19 +1,55 @@
-url = '/api/v1.0/civil_court_data'
+// Define the URL to fetch data from
+const url = '/api/v1.0/civil_court_data';
 
-d3.json(url).then(generateFullChart)
-
+// Fetch data from the API and populate the dropdown
+d3.json(url).then(generateFullChart)  
+    
 function generateFullChart(response) {
+   year = response.Year_Period
+   jurisdiction = response.Jurisdiction
+   case_type = response.Case_Type
+   case_count = response.Case_Count
 
-   
+   let trace1 = {
+      x: jurisdiction,
+      y: case_count,
+      text: "jurisdiction",
+      name: "Jurisdiction",
+      type: "bar"
+   }
 
-}
+   let trace2 = {
+      x: case_type,
+      y: case_count,
+      text: "case_type",
+      name: "Case Type",
+      type: "bar"
+   }
+
+   let data = [trace1, trace2]
+
+   let layout = {
+      title: "Number of Civil Cases by Jurisdiction and Case Type from 2005 to 2022",
+      barmode: "group",
+      margin: {
+         l: 50,
+         r: 50,
+         b: 200,
+         t: 50,
+         pad: 4
+       }
+   }
+
+   Plotly.newPlot("plot", data, layout)
+};
+
 
 d3.json(url).then(generateCaseTypeChart)
 
 function generateCaseTypeChart(response) {
 
    
-}
+};
 
 
 d3.json(url).then(generateJurisdictionChart)
@@ -21,4 +57,4 @@ d3.json(url).then(generateJurisdictionChart)
 function generateJurisdictionChart(response) {
 
    
-}
+};
